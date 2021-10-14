b577c46a172973d9091b0c9d1d8a37181d235cf6
AndrePina6577 <pina.a.andre@gmail.com>
2021-10-14 20:57:58 +0100

    Simple hosted service

--- WorkerServiceExample/Worker.cs
+++ WorkerServiceExample/Worker.cs
@@ -8,22 +8,60 @@ using System.Threading.Tasks;
 
 namespace WorkerServiceExample
 {
-    public class Worker : BackgroundService
+    public class Worker : IHostedService
     {
         private readonly ILogger<Worker> _logger;
+        private Task task;
+        private readonly CancellationTokenSource tokenSource = new CancellationTokenSource();
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        public Task StartAsync(CancellationToken cancellationToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            //this will keep running until cancelation is requested
+            task = SomeJob(tokenSource.Token);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (task == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                tokenSource.Cancel();
+            }
+            finally
+            {
+                Task.WhenAny(task, Task.Delay(Timeout.Infinite, cancellationToken));
+            }
+
+            return Task.CompletedTask;
+        }
+
+        protected async Task SomeJob(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(1000, token);
             }
         }
+
+        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //{
+        //    while (!stoppingToken.IsCancellationRequested)
+        //    {
+        //        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        //        await Task.Delay(1000, stoppingToken);
+        //    }
+        //}
     }
 }
